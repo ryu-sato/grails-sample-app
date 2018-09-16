64f26e406e61fcc00200531642ef88f270e56cd1
Ryu Sato <ryu@weseek.co.jp>
2018-09-16 14:18:49 +0900

    Employee の CRUD 用 controller と view を追加

--- src/test/groovy/grails/sample/app/EmployeeSpec.groovy
+++ src/test/groovy/grails/sample/app/EmployeeSpec.groovy
@@ -5,14 +5,14 @@ import spock.lang.Specification
 
 class EmployeeSpec extends Specification implements DomainUnitTest<Employee> {
 
-  def setup() {
-  }
+    def setup() {
+    }
 
-  def cleanup() {
-  }
+    def cleanup() {
+    }
 
-  void "test something"() {
-    expect:"fix me"
-      true == false
-  }
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
 }
